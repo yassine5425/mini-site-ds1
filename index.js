767b9116ae73d193425e182b1afe77bd7b2d4264
yassine <benafiayassine0@gmail.com>
2025-04-20 16:11:26 +0100

    Ajout de la fonctionnalité JavaScript - modification-js-feature

---
+++ index.js
@@ -0,0 +1,9 @@
+// Ceci est un fichier script.js
+
+// Exemple de fonction qui affiche un message
+function afficherMessage() {
+    alert("Bonjour, c'est mon premier code JavaScript !");
+}
+
+// Appel de la fonction
+afficherMessage();
