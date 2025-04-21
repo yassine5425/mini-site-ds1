c33badb11d3a9563fa75ccb7425f7273d5f8b093
yassine <benafiayassine0@gmail.com>
2025-04-21 02:10:09 +0100

    Ajout du fichier script.js

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+// Exemple de fonction qui affiche un message
+function afficherMessage() {
+    alert("Bonjour, c'est mon premier code JavaScript !");
+}
+
+// Appel de la fonction
+afficherMessage();
